a85f61052bf44776d82991634c7a79a15619862f
wubingheng <wubinghengajw@outllok.com>
2019-09-09 03:43:35 +0800

    update ScheduleTest.

--- src/main/java/cn/net/nigel/concurrent/ScheduleTest2.java
+++ src/main/java/cn/net/nigel/concurrent/ScheduleTest2.java
@@ -1,32 +1,54 @@
 package cn.net.nigel.concurrent;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class ScheduleTest2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 //        ScheduledThreadPoolExecutor executor;
-        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(2);
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(3);
         executorService.scheduleWithFixedDelay(() -> {
             try {
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("scheduleWithFixedDelay");
+            System.out.println("scheduleWithFixedDelay: " + System.currentTimeMillis());
         }, 0, 2, TimeUnit.SECONDS);
 
+        ScheduledFuture future =
         executorService.scheduleAtFixedRate(() -> {
             try {
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("scheduleAtFixedRate");
+            System.out.println("scheduleAtFixedRate: " + System.currentTimeMillis());
         }, 0, 2, TimeUnit.SECONDS);
+
+//        executorService.scheduleAtFixedRate(() -> {
+//            try {
+//                Thread.sleep(5000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println("scheduleAtFixedRate2: " + System.currentTimeMillis());
+//        }, 0, 2, TimeUnit.SECONDS);
+
+//            try {
+//                System.out.println(future.get());
+//            } catch (InterruptedException | ExecutionException e) {
+//                e.printStackTrace();
+//            }
+//      # 1
+//        Thread.sleep(1000);
 //        executorService.shutdown();
+//      # 2
+//        while (future.isDone()) {
+//            System.out.println(future.isDone());
+//            System.out.println(future.isCancelled());
+//            Thread.sleep(1000);
+//        }
+//      # 3
     }
 }
